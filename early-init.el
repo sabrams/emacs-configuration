3c7c5dfbe32fe35b03dfb8adf194a90e60dfcf57
Stephen Abrams <abrams.stephen@gmail.com>
2024-11-20 14:12:36 -0500

    Courier Prime font is used

--- early-init.el
+++ early-init.el
@@ -2,3 +2,13 @@
 
 ;; Disable the built-in package manager because Elpaca is used instead
 (setq package-enable-at-startup nil)
+
+(setq c/monospace-font "Courier Prime"
+      c/monospace-font-size 14)
+
+;; Configure default frame settings before the first frame is shown
+(setq default-frame-alist
+      (append
+       (list
+        `(font . ,(concat c/monospace-font "-" (number-to-string c/monospace-font-size))))
+       default-frame-alist))

72eef35a1252421587f8df1e0946f30ed95fac5c
Stephen Abrams <abrams.stephen@gmail.com>
2026-01-20 12:29:23 -0500

    JetBrainsMono Nerd Font

--- early-init.el
+++ early-init.el
@@ -3,7 +3,7 @@
 ;; Disable the built-in package manager because Elpaca is used instead
 (setq package-enable-at-startup nil)
 
-(setq c/monospace-font "Courier Prime"
+(setq c/monospace-font "JetBrainsMono Nerd Font"
       c/monospace-font-size 14)
 
 ;; Configure default frame settings before the first frame is shown

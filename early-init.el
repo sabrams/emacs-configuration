055f7266f43da326c3cbf93e26de7354354a01e6
Stephen Abrams <abrams.stephen@gmail.com>
2026-01-30 17:03:15 -0500

    Monaspace Neon with ligatures and GitHub dark theme

--- early-init.el
+++ early-init.el
@@ -3,12 +3,14 @@
 ;; Disable the built-in package manager because Elpaca is used instead
 (setq package-enable-at-startup nil)
 
-(setq c/monospace-font "JetBrainsMono Nerd Font"
-      c/monospace-font-size 18)
+(setq c/monospace-font "Monaspace Neon"
+      c/monospace-font-size 16)
 
 ;; Configure default frame settings before the first frame is shown
 (setq default-frame-alist
       (append
        (list
-        `(font . ,(concat c/monospace-font "-" (number-to-string c/monospace-font-size))))
+        `(font . ,(format "%s:size=%d:weight=light:width=normal"
+                          c/monospace-font
+                          c/monospace-font-size)))
        default-frame-alist))
